7a46bc48c049412bb070d4fd5e3ad5a367fb3d65
Valeriy <valeriy.all@gmail.com>
2023-03-01 15:57:53 +0200

    1 + 2 tasks

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingridientsContainerEl = document.querySelector('ul');
+const creatUl = ingredients.map(ingridient => {
+ 
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingridient;
+  
+  return liEl
+});
+
+console.log(creatUl)
+
+ingridientsContainerEl.append(...creatUl);
+
+
